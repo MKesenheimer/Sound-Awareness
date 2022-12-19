846cd3b8dba88d3bc9acb3414fd8ab21deb244fe
MKesenheimer <mkesenheimer@gmx.net>
2022-12-19 14:36:49 +0100

    Working as needed

--- Main.cpp
+++ Main.cpp
@@ -19,28 +19,38 @@
 #include "SDLTools/CommandLineParser.h"
 
 
-uint8_t threshold = 0xB0;
+uint8_t gThreshold = 0x88;
+Uint8* gRecordingBuffer = NULL;
+Uint32 gBufferByteSize = 0;
+static bool monitor = true;
+static bool run = true;
 
+// TODO
 void usage(char* argv[]) {
     std::cout << "Usage:" << std::endl << argv[0] << " -t <threshold> [options]" << std::endl;
     std::exit(-1);
 }
 
-static int getKeyboardInput() {
-    std::string input = sdl::auxiliary::ccin<std::string>("[USER] -/+: decrease/increase threshold, q: quit: ");
-    if (input == "-") {
-        std::cout << "[INFO] Decreasing threshold." << std::endl;
-        threshold -= 10;
-        std::cout << "[INFO] Threshold = " << std::hex << std::setfill('0') << std::setw(2) << std::right << static_cast<unsigned int>(threshold) << std::dec << std::endl;
-    } else if (input == "+") {
-        std::cout << "[INFO] Increasing threshold." << std::endl;
-        threshold += 10;
-        std::cout << "[INFO] Threshold = " << std::hex << std::setfill('0') << std::setw(2) << std::right << static_cast<unsigned int>(threshold) << std::dec << std::endl;
-    } else if (input == "q") {
-        std::cout << "[INFO] Exiting" << std::endl;
-        return 1;
+static void getKeyboardInput() {
+    while (true) {
+        std::string input = sdl::auxiliary::ccin<std::string>("[USER] -/+: decrease/increase threshold, q: quit: ");
+        if (input == "-") {
+            std::cout << "[INFO] Decreasing threshold." << std::endl;
+            if (gThreshold >= 5)
+                gThreshold -= 5;
+            std::cout << "[INFO] Threshold = " << std::hex << std::setfill('0') << std::setw(2) << std::right << static_cast<unsigned int>(gThreshold) << std::dec << std::endl;
+        } else if (input == "+") {
+            std::cout << "[INFO] Increasing threshold." << std::endl;
+            if (gThreshold <= 250)
+                gThreshold += 5;
+            std::cout << "[INFO] Threshold = " << std::hex << std::setfill('0') << std::setw(2) << std::right << static_cast<unsigned int>(gThreshold) << std::dec << std::endl;
+        } else if (input == "q") {
+            std::cout << "[INFO] Exiting" << std::endl;
+            run = false;
+            monitor = false;
+            break;
+        }
     }
-    return 0;
 }
 
 void audioRecordingCallback(void* userdata, Uint8* stream, int len) {
@@ -53,21 +63,39 @@ void audioRecordingCallback(void* userdata, Uint8* stream, int len) {
     numberOfSamples += len;
 #endif
 
-    for (int i = 0; i < len; ++i)
-        if (static_cast<unsigned int>(stream[i]) >= threshold) {
-            //std::cout << "ALARM!" << std::endl;
+    for (int i = 0; i < len; ++i) {
+        if (static_cast<unsigned int>(stream[i]) >= gThreshold) {
             system("osascript -e 'display notification \"Something loud is happening!\" with title\"Alarm!\" subtitle \"Notification from sound-awareness daemon\"'");
+
+            // store the buffer for later output
+            if (gBufferByteSize == len && gRecordingBuffer != NULL) {
+                std::memcpy(gRecordingBuffer, stream, len);
+            } else {
+                if (gRecordingBuffer != NULL)
+                    delete[] gRecordingBuffer;
+                gBufferByteSize = len;
+                gRecordingBuffer = new Uint8[len];
+                std::memcpy(gRecordingBuffer, stream, len);
+            }
+
+            monitor = false;
             break;
         }
+    }
 }
 
 void audioPlaybackCallback(void* userdata, Uint8* stream, int len) {
-    /*//Copy audio to stream
-    std::memcpy(stream, &gRecordingBuffer[ gBufferBytePosition ], len);
+    // copy audio to stream
+    if (gBufferByteSize >= len && gRecordingBuffer != NULL) {
+#if 0
+        for (int i = 0; i < len; ++i)
+            std::cout << std::hex << std::setfill('0') << std::setw(2) << std::right << static_cast<unsigned int>(gRecordingBuffer[i]);
+        std::cout << std::dec << std::endl;
+#endif
 
-    //Move along buffer
-    gBufferBytePosition += len;
-    */
+        std::memcpy(stream, gRecordingBuffer, len);
+    }
+    monitor = true;
 }
 
 int main(int argc, char* argv[]) {
@@ -83,7 +111,7 @@ int main(int argc, char* argv[]) {
     desiredRecordingSpec.freq = 44100;
     desiredRecordingSpec.format = AUDIO_U8;
     desiredRecordingSpec.channels = 2;
-    desiredRecordingSpec.samples = 4096;
+    desiredRecordingSpec.samples = 16384;
     desiredRecordingSpec.callback = audioRecordingCallback;
     int recordingDeviceCount = SDL_GetNumAudioDevices(SDL_TRUE);
     if(recordingDeviceCount < 1) {
@@ -119,7 +147,7 @@ int main(int argc, char* argv[]) {
     desiredPlaybackSpec.freq = 44100;
     desiredPlaybackSpec.format = AUDIO_U8; // AUDIO_F32;
     desiredPlaybackSpec.channels = 2;
-    desiredPlaybackSpec.samples = 4096;
+    desiredPlaybackSpec.samples = 16384;
     desiredPlaybackSpec.callback = audioPlaybackCallback;
 
     // Open playback device
@@ -127,31 +155,47 @@ int main(int argc, char* argv[]) {
     playbackDeviceId = SDL_OpenAudioDevice(NULL, SDL_FALSE, &desiredPlaybackSpec, &receivedPlaybackSpec, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
     //Device failed to open
     if(playbackDeviceId == 0) {
-        //Report error
         std::cerr << "[ERROR] Failed to open playback device! SDL Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-
-    // monitoring
+    // handle keyboard input in a separate thread
+    std::thread t(getKeyboardInput);
+    
     std::cout << "[INFO] Monitoring" << std::endl;
-    SDL_PauseAudioDevice(recordingDeviceId, SDL_FALSE);
-    bool run = true;
-    while (run) { //(numberOfSamples == 0) {
-        // Lock callback
-        SDL_LockAudioDevice(recordingDeviceId);
+    while (run) {
+        // monitoring
+        SDL_PauseAudioDevice(recordingDeviceId, SDL_FALSE);
+        while (monitor) {
+            // Lock callback
+            SDL_LockAudioDevice(recordingDeviceId);
 
-        std::chrono::seconds timeout(10);
-        std::future<int> future = std::async(getKeyboardInput);
-        if (future.wait_for(timeout) == std::future_status::ready)
-            if(future.get() == 1)
-                run = false;
+            // Unlock callback
+            SDL_UnlockAudioDevice(recordingDeviceId);
+        }
+        // stop monitoring audio
+        SDL_PauseAudioDevice(recordingDeviceId, SDL_TRUE);
 
-        // Unlock callback
-        SDL_UnlockAudioDevice(recordingDeviceId);
+        // playback
+        SDL_PauseAudioDevice(playbackDeviceId, SDL_FALSE);
+        while (!monitor) {
+            // Lock callback
+            SDL_LockAudioDevice(playbackDeviceId);
+
+            // Unlock callback
+            SDL_UnlockAudioDevice(playbackDeviceId);
+        }
+        // stop playback audio
+        SDL_PauseAudioDevice(playbackDeviceId, SDL_TRUE);
     }
-    // stop monitoring audio
-    SDL_PauseAudioDevice(recordingDeviceId, SDL_TRUE);
+
+    // join and terminate thread
+    t.join();
+
+    if(gRecordingBuffer != NULL) {
+        delete[] gRecordingBuffer;
+        gRecordingBuffer = NULL;
+    }
 
     SDL_Quit();
     return 0;
